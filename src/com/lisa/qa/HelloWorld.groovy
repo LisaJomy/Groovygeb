3bf9d06c900d42f9f84310deea8482dbb3060139
Lisa Jomy <lisa.selenium@gmail.com>
2020-08-24 23:20:10 -0400

    changes

--- src/com/lisa/qa/HelloWorld.groovy
+++ src/com/lisa/qa/HelloWorld.groovy
@@ -1,2 +1,21 @@
 package com.lisa.qa
-println"Hello world"
+/*
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+*/
+for (String str : stringarray) {
+
+}
+
+public void method() {
+    system.out.println(First) line ");
+    system.out.println("Third line")
+
+}
