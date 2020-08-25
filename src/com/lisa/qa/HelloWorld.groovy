382c8bd97203a2a2f7edb5e8c69ff661855d3cb9
LisaJomy <56934912+LisaJomy@users.noreply.github.com>
2020-08-24 23:23:16 -0400

    Update HelloWorld.groovy

--- src/com/lisa/qa/HelloWorld.groovy
+++ src/com/lisa/qa/HelloWorld.groovy
@@ -1,21 +1,2 @@
 package com.lisa.qa
-/*
-println "Hello World"
-package com.lisa.qa
-println "Hello World"
-package com.lisa.qa
-println "Hello World"
-package com.lisa.qa
-println "Hello World"
-package com.lisa.qa
-println "Hello World"
-*/
-for (String str : stringarray) {
-
-}
-
-public void method() {
-    system.out.println(First) line ");
-    system.out.println("Third line")
-
-}
+println "I am master"
