055eb5f64215aad9acaa909ee62e67adedf500ba
Lisa Jomy <lisa.selenium@gmail.com>
2020-08-24 23:02:08 -0400

    master

---
+++ src/com/lisa/qa/HelloWorld.groovy
@@ -0,0 +1,21 @@
+package com.lisa.qa
+/*
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+package com.lisa.qa
+println "Hello World"
+*/
+for (String str : stringarray) {
+
+}
+
+public void method() {
+    system.out.println(First) line ");
+    system.out.println("Third line")
+
+}
